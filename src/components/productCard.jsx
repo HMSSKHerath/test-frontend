87b383588b65909ee881653143feecb89a38b9f2
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-03-24 18:56:18 +0530

    Fix image URL and update class names in ProductCard component

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -5,11 +5,10 @@ export default function ProductCard(props)
     console.log(props);
 
     return(
-        <div className="productCard">
+        <div className="product-card">
             <h1>{props.name}</h1>
-            <p>Price: {props.price}</p>
-            <img className="productImage" src={props.image} alt={props.name}/>
-            <button>Add to Cart</button>
+            <p>{props.price}</p>
+            <img src={props.image} alt={"Product Image"}/>
         </div>
     );
 } 
