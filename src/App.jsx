c6ee9d5ad5f61c042492c1271e1aea0b51230617
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-03-25 19:56:22 +0530

    Update ProductCard styles and structure; enhance title-bar and image dimensions

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1 className="title-bar">Sasidu</h1>
+      <h1 className="title-bar name-bar">Sasidu</h1>
       <ProductCard name="Apple Ipad" price="$399.99" image="https://d2bschjhk4kxui.cloudfront.net/assets/images/product/69a6b033c3db51772531763.webp"/>
       <ProductCard name="Samsung Galaxy" price="$299.99" image="https://img.drz.lazcdn.com/static/lk/p/ce5349923102af31de5e2d408588c436.jpg"/>
     </>
--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -5,10 +5,11 @@ export default function ProductCard(props)
     console.log(props);
 
     return(
-        <div className="product-card">
+        <div className="productCard">
             <h1>{props.name}</h1>
             <p>{props.price}</p>
-            <img src={props.image} alt={"Product Image"}/>
+            <img className="productImage" src={props.image} alt={"Product Image"}/>
+            <button>Add to Cart</button>
         </div>
     );
 } 
