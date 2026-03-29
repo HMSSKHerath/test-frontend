e6fd5e068c755a4fc06ac6f7045f4ef7ee8110a4
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-03-29 23:17:25 +0530

    feat: add TestPage component and integrate routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import { BrowserRouter,Routes,Route} from 'react-router-dom'
 import AdminPage from './pages/adminPage'
 import HomePage from './pages/homePage'
+import TestPage from './pages/test'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <div className='w-full h-screen bg-red-600'> 
+      <div className='w-full h-screen bg-accent'> 
         <Routes>
           <Route path='/*' element={<HomePage />} />
           <Route path='/admin/*' element={<AdminPage />} />
+          <Route path='/test' element={<TestPage />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/pages/test.jsx
@@ -0,0 +1,26 @@
+export default function TestPage()
+{
+
+    let count = 10;
+
+    return(
+        <div className="w-full h-full text-2xl flex items-center justify-center">
+            <div className="w-[500px] h-[500px] bg-blue-300  text-white flex items-center justify-center gap-[30px]">
+                <button className="w-[100px] h-[100px] bg-red-600 rounded-lg" onClick={
+                    ()=>{
+                        console.log("Decreasing.....");
+                        count--; 
+                        console.log(count);                    }
+                }>-</button>
+                <span className="text-8xl">{count}</span>
+                <button className="w-[100px] h-[100px] bg-red-600 rounded-lg" onClick={
+                    ()=>{
+                        console.log("Increasing.....");
+                        count++;
+                        console.log(count);
+                    }
+                }>+</button>
+            </div>
+        </div>
+    )
+}
