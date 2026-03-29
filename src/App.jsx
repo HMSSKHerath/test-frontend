1b6b68e8b7ac55dcb5882c62a293de5440831c71
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-03-29 20:35:08 +0530

    feat: enhance AdminPage layout and routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
       <div className='w-full h-screen bg-red-600'> 
         <Routes>
           <Route path='/' element={<h1>Home Page</h1>} />
+          <Route path='/admin/*' element={<AdminPage />} />
           <Route path='/about' element={<h1>About Page</h1>} />
           <Route path='/contact' element={<h1>Contact Page</h1>} />
           <Route path='/login' element={<h1>Login Page</h1>} />
           <Route path='/register' element={<h1>Register Page</h1>} />
-          <Route path='/admin' element={<AdminPage />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,6 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+
 export default function AdminPage() 
 {
     return (
-        <div className="w-full h-screen bg-blue-400"></div>
+        <div className="w-full h-screen bg-blue-900 flex p-2">
+            <div className="w-[300px] h-full bg-blue-900"></div>
+            <div className="w-[calc(100%-300px)] h-full bg-green-400 rounded-[20px]">
+                <Routes>
+                    <Route path='/' element={<h1>Admin Dashboard</h1>} />
+                    <Route path='/products' element={<h1>Product Management</h1>} />
+                    <Route path='/users' element={<h1>User Management</h1>} />
+                </Routes>
+            </div>
+        </div>
     )
 }
