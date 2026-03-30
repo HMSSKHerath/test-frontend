ca4a18380510872b151962e3a57da074b2f58173
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-03-30 11:31:01 +0530

    fix: update background color of TestPage component

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -5,14 +5,17 @@ export default function TestPage()
 
     return(
         <div className="w-full h-full text-2xl flex items-center justify-center">
-            <div className="w-[500px] h-[500px] bg-blue-300  text-white flex items-center justify-center gap-[30px]">
+            <div className="w-[500px] h-[500px] bg-pink-400  text-white flex items-center justify-center gap-[30px]">
                 <button className="w-[100px] h-[100px] bg-red-600 rounded-lg" onClick={
                     ()=>{
                         console.log("Decreasing.....");
                         count--; 
-                        console.log(count);                    }
+                        console.log(count);                    
+                    }
                 }>-</button>
+
                 <span className="text-8xl">{count}</span>
+
                 <button className="w-[100px] h-[100px] bg-red-600 rounded-lg" onClick={
                     ()=>{
                         console.log("Increasing.....");
