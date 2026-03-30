7b8ce9a944f264c3ea7ae46b871d744e6b0d5113
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-03-30 18:37:09 +0530

    fix: update initial count value and standardize button styles in TestPage component

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -2,23 +2,23 @@ import { useState } from "react";
 
 export default function TestPage()
 {
-    const [count,setCount] = useState(10);
+    const [count,setCount] = useState(50);
     const [status,setStatus] = useState("Online");
 
     return(
         <div className="w-full h-full flex items-center justify-center">
-            <div className="w-[500px] h-[500px] bg-pink-400  text-white flex flex-col items-center justify-center gap-[30px]">
-                <div className="flex items-center justify-center gap-[40px] text-[50px]">
-                    <button className="w-[100px] h-[100px] bg-red-600 rounded-lg" onClick={
+            <div className="w-[500px] h-[500px] bg-[#fff1c4] flex flex-col items-center justify-center gap-[30px]">
+                <div className="flex items-center justify-center gap-[40px] text-[50px] text-white">
+                    <button className="w-[100px] h-[100px] bg-accent rounded-lg" onClick={
                         ()=>{
                             console.log("Decreasing.....");
                             setCount(count-1 );
                         }
                     }>-</button>
 
-                    <span className="text-8xl">{count}</span>
+                    <span className="text-8xl text-black">{count}</span>
 
-                    <button className="w-[100px] h-[100px] bg-red-600 rounded-lg" onClick={
+                    <button className="w-[100px] h-[100px] bg-accent rounded-lg" onClick={
                         ()=>{
                             console.log("Increasing.....");
                             setCount(count+1);
@@ -26,24 +26,15 @@ export default function TestPage()
                     }>+</button>
                 </div>
 
-                <div className="flex flex-col items-center justify-center gap-[30px]">
-                    <span className="text-6xl">{status}</span>
+                <div className="flex flex-col items-center justify-center gap-[30px] text-white">
+                    <span className="text-6xl text-black">{status}</span>
 
                     <div className="flex flex-row gap-[20px] text-lg">
-                        <button className="w-[120px] h-[50px] bg-red-500 rounded-lg" onClick={
-                            ()=>{
-                                setStatus('Online');
-                            }
+                        <button className="w-[120px] h-[50px] bg-accent rounded-lg" onClick={()=>{setStatus('Online');}
                         }>Online</button>
-                        <button className="w-[120px] h-[50px] bg-red-500 rounded-lg" onClick={
-                            ()=>{
-                                setStatus('Offline');
-                            }
+                        <button className="w-[120px] h-[50px] bg-accent rounded-lg" onClick={()=>{setStatus('Offline');}
                         }>Offline</button>
-                        <button className="w-[120px] h-[50px] bg-red-500 rounded-lg" onClick={
-                            ()=>{
-                                setStatus('DeActivated');
-                            }
+                        <button className="w-[120px] h-[50px] bg-accent rounded-lg" onClick={()=>{setStatus('DeActivated');}
                         }>DeActivated</button>
                     </div>
 
